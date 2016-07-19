05b23d707c5f0d226baab44c4cdccaf3f8dd2d05
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-19 09:05:33 -0400

    Sandwichr: Add Sandwiches API actions

--- sandwichr/app/controllers/sandwiches_controller.rb
+++ sandwichr/app/controllers/sandwiches_controller.rb
@@ -1,2 +1,52 @@
 class SandwichesController < ApplicationController
+
+  def index
+    sandwiches_array = Sandwiches.all
+    render json: sandwiches_array
+  end
+
+  def create
+    the_sandwich = Sandwich.create(sandwich_params)
+    render json: the_sandwich
+  end
+
+  def show
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      render json: the_sandwich
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+  def update
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      the_sandwich.update(sandwich_params)
+      render json: the_sandwich
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+  def destroy
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      the_sandwich.destroy
+      render json: the_sandwich
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+
+  private
+
+  def sandwich_params
+    params.require(:sandwich).permit(:name, :bread_type)
+  end
+
 end
