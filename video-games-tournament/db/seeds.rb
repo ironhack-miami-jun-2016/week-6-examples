4201e772554beec26e6f97f12917dca676b76810
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-20 09:06:43 -0400

    Video Games: Add Tournament and Player models

--- video-games-tournament/db/seeds.rb
+++ video-games-tournament/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tourneyA = Tournament.create!(name: "Pokemon Catching Championship")
+tourneyB = Tournament.create!(name: "Smash Brothers Tournament")
+tourneyC = Tournament.create!(name: "Tetris Tournament")
+
+playerA = Player.create!(name: "Izzy Ironside")
+playerB = Player.create!(name: "Nizar")
+playerC = Player.create!(name: "Josh")
+playerD = Player.create!(name: "Nick")
+playerE = Player.create!(name: "Alia")
+playerF = Player.create!(name: "Ariel")
+playerG = Player.create!(name: "Alida")
+playerH = Player.create!(name: "Daniel")
+
+
+tourneyA.players.push( playerA )
+tourneyA.players.push( playerB )
+tourneyA.players.push( playerC )
+tourneyA.players.push( playerD )
+
+tourneyB.players.push( playerE )
+tourneyB.players.push( playerF )
+tourneyB.players.push( playerG )
+tourneyB.players.push( playerH )
